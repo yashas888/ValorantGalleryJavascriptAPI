d6e734df99b596e8d8b5336e802aaf858445a8b9
yashas888 <yashaskumar888@gmail.com.com>
2023-02-02 21:45:29 +0530

    view by category

--- index.js
+++ index.js
@@ -1,39 +1,47 @@
+const api_url =
+    "https://valorant-api.com/v1/agents";
 console.log("hello");
 let desc = document.querySelector(".card-text")
 let aname = document.querySelector(".card-title")
 let image = document.querySelector(".card-img-top")
 let agentcards = document.querySelector(".agent-cards")
+
 let btn = document.getElementsByTagName('a');
 
+// fetching data as soon as window loads
 
-const api_url =
-"https://valorant-api.com/v1/agents";
-
-
-// fetching data
-fetch(api_url)
-.then((response) => response.json())
-.then((value) => {
-    let arr = Array.from(value.data);
-    arr.forEach((ele) => {
-        if (ele.isPlayableCharacter == true) {
-            agentcards.innerHTML += getHtml(ele.fullPortrait, ele.displayName, ele.uuid);
-            console.log(`${api_url}/${ele.uuid}`)
+window.onload = () =>{
+    fetch(api_url)
+    .then((response) => response.json())
+    .then((value) => {
+        let arr = Array.from(value.data);
+        arr.forEach((ele) => {
+            if (ele.isPlayableCharacter == true) {
+                agentcards.innerHTML += getHtml(ele.fullPortrait, ele.displayName, ele.uuid);
+                // console.log(`${api_url}/${ele.uuid}`)
+                // console.log(ele.role.displayName)
+            }
+        })
+        // <p class="card-text">${description}</p>
+        function getHtml(fullPortrait, displayName, uuid) {
+            return `<div class="card" style="width: 25rem; height:500px">
+            <img src="${fullPortrait}" class="card-img-top" alt="...">
+            <div class="card-body">
+            <h5 class="card-title text-center">${displayName}</h5>
+            <a href="details.html?id=${uuid}" class="btn btn-primary d-flex justify-content-center">View Agent Details</a>
+            </div>
+            </div>`
         }
-    })
-    // <p class="card-text">${description}</p>
-    function getHtml(fullPortrait, displayName, uuid) {
-        return `<div class="card" style="width: 25rem; height:500px">
-        <img src="${fullPortrait}" class="card-img-top" alt="...">
-        <div class="card-body">
-        <h5 class="card-title text-center">${displayName}</h5>
-        <a href="details.html?id=${uuid}" class="btn btn-primary d-flex justify-content-center">View Agent Details</a>
-        </div>
-        </div>`
-    }
-});
+    });
+}
 
-    
-    
+
+
+
+
+
+
+
+
 
 
